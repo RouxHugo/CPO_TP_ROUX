43c2b93a6f7c26a70b9de8b80bf76b5684487ca0
hugo roux <hugo.roux1@epfedu.fr>
2024-09-17 16:12:35 +0200

    réalisation de l'exo 1 du TP 0

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,33 @@
+/*
+ * TPG exercice1
+ * MRG
+ * 17/09/2024   
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hugor 17/09/2024
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("au revoir");  
+        
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("kwukwu, quel est ton prenom ?");
+        prenom = sc.nextLine();
+
+    }
+    
+}
