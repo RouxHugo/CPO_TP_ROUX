f5436817804fd42e95f2394c91207fd6739d52dc
hugo roux <hugo.roux1@epfedu.fr>
2024-10-11 12:59:03 +0200

    réalisation de l'exo 2 du TP 2

--- TP2_convertisseurObjet_ROUX/src/tp2_convertisseurobjet_roux/Convertisseur.java
+++ TP2_convertisseurObjet_ROUX/src/tp2_convertisseurobjet_roux/Convertisseur.java
@@ -14,34 +14,39 @@ public class Convertisseur {
 public Convertisseur () {
  nbConversions = 0 ;
 }    
-public static double CelciusVersKelvin(double tCelcius) {
+public  double CelciusVersKelvin(double tCelcius) {
+        nbConversions ++ ;
         return tCelcius + 273.15; // Conversion de Celsius en Kelvin
     }
 
     // Méthode de conversion de Kelvin vers Celsius
-public static double KelvinVersCelcius(double tKelvin) {
+public  double KelvinVersCelcius(double tKelvin) {
+    nbConversions ++ ;
         return tKelvin - 273.15;
     }
 
     // Méthode de conversion de Fahrenheit vers Celsius
-public static double FarenheitVersCelcius(double tFahrenheit) {
+public  double FarenheitVersCelcius(double tFahrenheit) {
+    nbConversions ++ ;
         return (tFahrenheit - 32) * 5 / 9;
     }
 
     // Méthode de conversion de Celsius vers Fahrenheit
-public static double CelciusVersFarenheit(double tCelcius) {
+public  double CelciusVersFarenheit(double tCelcius) {
+    nbConversions ++ ;
         return (tCelcius * 9 / 5) + 32;
     }
 
     // Méthode de conversion de Kelvin vers Fahrenheit (utilisant deux autres méthodes)
-public static double KelvinVersFarenheit(double tKelvin) {
+public  double KelvinVersFarenheit(double tKelvin) {
+    
         // Convertit d'abord Kelvin vers Celsius, puis Celsius vers Fahrenheit
         double tCelcius = KelvinVersCelcius(tKelvin);
         return CelciusVersFarenheit(tCelcius);
     }
 
     // Méthode de conversion de Fahrenheit vers Kelvin (utilisant deux autres méthodes)
-public static double FarenheitVersKelvin(double tFahrenheit) {
+public  double FarenheitVersKelvin(double tFahrenheit) {
         // Convertit d'abord Fahrenheit vers Celsius, puis Celsius vers Kelvin
         double tCelcius = FarenheitVersCelcius(tFahrenheit);
         return CelciusVersKelvin(tCelcius);
