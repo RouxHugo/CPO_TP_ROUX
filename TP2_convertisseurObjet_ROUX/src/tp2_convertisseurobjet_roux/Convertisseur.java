b7cb635a75cc0f1007e10ab7654c106f381c5a8a
hugo roux <hugo.roux1@epfedu.fr>
2024-10-11 12:44:46 +0200

    réalisation de l'exo 2 du TP 2

---
+++ TP2_convertisseurObjet_ROUX/src/tp2_convertisseurobjet_roux/Convertisseur.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_roux;
+
+/**
+ *
+ * @author hugor
+ */
+public class Convertisseur {
+    int nbConversions ;
+    
+public Convertisseur () {
+ nbConversions = 0 ;
+}    
+public static double CelciusVersKelvin(double tCelcius) {
+        return tCelcius + 273.15; // Conversion de Celsius en Kelvin
+    }
+
+    // Méthode de conversion de Kelvin vers Celsius
+public static double KelvinVersCelcius(double tKelvin) {
+        return tKelvin - 273.15;
+    }
+
+    // Méthode de conversion de Fahrenheit vers Celsius
+public static double FarenheitVersCelcius(double tFahrenheit) {
+        return (tFahrenheit - 32) * 5 / 9;
+    }
+
+    // Méthode de conversion de Celsius vers Fahrenheit
+public static double CelciusVersFarenheit(double tCelcius) {
+        return (tCelcius * 9 / 5) + 32;
+    }
+
+    // Méthode de conversion de Kelvin vers Fahrenheit (utilisant deux autres méthodes)
+public static double KelvinVersFarenheit(double tKelvin) {
+        // Convertit d'abord Kelvin vers Celsius, puis Celsius vers Fahrenheit
+        double tCelcius = KelvinVersCelcius(tKelvin);
+        return CelciusVersFarenheit(tCelcius);
+    }
+
+    // Méthode de conversion de Fahrenheit vers Kelvin (utilisant deux autres méthodes)
+public static double FarenheitVersKelvin(double tFahrenheit) {
+        // Convertit d'abord Fahrenheit vers Celsius, puis Celsius vers Kelvin
+        double tCelcius = FarenheitVersCelcius(tFahrenheit);
+        return CelciusVersKelvin(tCelcius);
+    }
+@Override
+public String toString () {
+ return "nb de conversions"+ nbConversions;
+}
+}
+    
