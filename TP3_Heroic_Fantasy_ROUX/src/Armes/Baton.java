1039c81b90155e327a451983b8ad83673cf5852e
hugo roux <hugo.roux1@epfedu.fr>
2024-11-20 10:30:41 +0100

    realisation du TP 3 exercice 1

---
+++ TP3_Heroic_Fantasy_ROUX/src/Armes/Baton.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Armes;
+
+/**
+ *
+ * @author hugor
+ */
+public class Baton extends Arme {
+    int age;
+
+    public Baton(String nom, int Attaque, int age) {
+        super(nom, Attaque);
+        this.age = Math.max(0,Math.min(age, 100));
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", Age : " + age;
+    }
+
+    public int getNiveauAttaque() {
+        return Attaque;
+    }
+
+    // Getter pour l'âge du bâton
+    public int getAge() {
+        return age;
+    }
+}
